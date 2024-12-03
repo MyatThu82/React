71092763a912c12ab0550d84cbdec659dc1be49e
MyatThu82 <147032158+MyatThu82@users.noreply.github.com>
2024-12-03 14:44:08 +0630

    Welcome message

---
+++ my-vite-app/src/Welcomeone.jsx
@@ -0,0 +1,12 @@
+function Welcomeone(){
+    return(
+        <section>
+            <h2>React Introduction</h2>
+            <p>
+                Hello React.
+            </p>
+        </section>
+    );
+}
+
+export default Welcomeone;
---
+++ my-vite-app/src/welcome.jsx
@@ -0,0 +1,12 @@
+function welcome(){
+    return(
+        <section>
+            <h2>React Introduction</h2>
+            <p>
+                Hello React.
+            </p>
+        </section>
+    );
+}
+
+export default welcome;
